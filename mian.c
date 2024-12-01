691e6f0ebf986d67be93fa08ddcde20c794cc5a5
Carl <1043151779@qq.com>
2024-12-01 18:09:34 +0800

    第一次上传文件

---
+++ mian.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() 
+{   int a;
+    printf("Hello, World!\nhello, DGUT!\n");
+ /*
+    printf("INPUT A NUM FROM 1~9\nPLEASE INPUT()\n");
+   scanf("%d",&a);
+  
+    printf("you INPUT %d",a);
+*/
+    return 0;
+}
