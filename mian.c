c2976465a178c8d3542caec38d8b66d61e5835de
Carl <1043151779@qq.com>
2024-12-02 20:23:05 +0800

    测试提交

---
+++ mian.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() 
+{   int a;
+    printf("445566778888888888999999Hello, World!\nhello, DGUT!\n");
+
+    printf("INPUT A NUM FROM 1~9\nPLEASE INPUT()\n");
+ /*
+   scanf("%d",&a);
+  
+    printf("you INPUT %d",a);
+*/
+    return 0;
+}
